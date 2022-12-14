52868eb0be2b8c0fef18e878e2a3a97b27aec08c
Biggestheart <57988686+Biggestheart@users.noreply.github.com>
2022-12-14 16:06:11 +0100

    Add files via upload
    Nothing to see... Here's my first Custom Function of python telling the World to be radical today.  😆 😆

---
+++ FILE_NAME.py
@@ -0,0 +1,13 @@
+def hello():
+   print("Hello world!")
+ 
+hello()
+hello()
+hello()
+
+def be_rad():
+  rad_string = " Be radical today. "
+  rad_x_100 = (rad_string * 100)
+  print(rad_x_100)
+  
+be_rad()
